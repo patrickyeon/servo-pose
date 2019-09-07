16af615e1d81666ce3a47867e8dc0842a0099b2e
Patrick Yeon <patrickyeon@gmail.com>
2019-09-06 19:25:37 -0700

    something that almost sort of works

--- servooo.ino
+++ servooo.ino
@@ -6,8 +6,8 @@ Servo joints[] = {shoulder, elbow, wrist};
 const int threshold = 60;
 const int vel = 2;
 
-int target[] = {90, 90, 90};
-int norm_read[] = {40, 40, 40};
+int targets[] = {90, 90, 90};
+int norm_read[] = {400, 90, 5};
 
 void setup() {
   Serial.begin(57600);
@@ -15,9 +15,19 @@ void setup() {
   shoulder.attach(9);
   elbow.attach(10);
   wrist.attach(11);
+  
+  pinMode(12, INPUT_PULLUP);
+  
+  for (int i = 0; i < 3; i++) {
+    joints[i].write(targets[i]);
+  }
+  
+  delay(1000);
+  
+  set_norms();
 }
 
-int last_reads[] = [0, 0, 0];
+int last_reads[] = {0, 0, 0};
 boolean cw[] = {true, true, true};
 
 void joint_step(int j) {
@@ -26,26 +36,37 @@ void joint_step(int j) {
     if (this_read > last_reads[j] + 40) {
       cw[j] = cw[j] ? false : true;
     }
-    int target = target[j];
+    int target = targets[j];
     if (cw[j]) {
       target += vel;
     } else {
       target -= vel;
     }
     last_reads[j] = this_read;
-    target[j] = min(180, max(0, target));
-    joints[j].write(target[j]);
+    targets[j] = min(180, max(0, target));
+    joints[j].write(targets[j]);
+  }
+}
+
+void set_norms() {
+  for (int i = 0; i < 3; i++) {
+    norm_read[i] = analogRead(i);
+    last_reads[i] = norm_read[i];
   }
 }
 
 void loop() {
-  shoulder.write(90);
-  elbow.write(90);
-  wrist.write(90);
-  Serial.print(analogRead(0));
-  Serial.print("/");
-  Serial.print(analogRead(1));
-  Serial.print("/");
-  Serial.println(analogRead(2));
-  delay(300);
+  boolean needs_cal = false;
+  while (digitalRead(12) == LOW) {
+    needs_cal = true;
+    delay(50);
+  }
+  if (needs_cal) {
+    set_norms();
+  }
+    
+  for (int i = 0; i < 3; i++) {
+    joint_step(i);
+  }
+  delay(100);
 }
